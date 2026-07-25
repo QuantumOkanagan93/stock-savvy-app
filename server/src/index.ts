029123b9fc7e0ca89cc7feda3cb09024c4ebe1fe
QuantumOkanagan93 <micheal.egan93@gmail.com>
2026-07-24 22:15:01 -0700

    Testing Prisma Configs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { prisma } from "./lib/prisma";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/health", (_req, res) => {
+app.get("/health", async (_req, res) => {
+    const userCount = await prisma.user.count();
     res.json({ status: "ok"});
 });
 
