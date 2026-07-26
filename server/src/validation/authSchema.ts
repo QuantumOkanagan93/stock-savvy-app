2dfae0fad90c4bfae211d737696173c2eef379f0
QuantumOkanagan93 <micheal.egan93@gmail.com>
2026-07-25 20:45:42 -0700

    more auth

---
+++ server/src/validation/authSchema.ts
@@ -0,0 +1,17 @@
+import type { sign } from "node:crypto";
+import { z } from "zod";
+
+export const signupSchema = z.object({
+    email: z.string().email("Must be a valid email address"),
+    password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long"),
+});
+
+export const loginSchema = z.object({
+    email: z.string().email("Must be a valid email address"),
+    password: z.string().min(1, "Password is required"),
+});
+
+export type SignupInput = z.infer<typeof signupSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
