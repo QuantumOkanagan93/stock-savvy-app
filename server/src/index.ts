08fcdada7262cc701af6c70f0f6e4fb888aac162
QuantumOkanagan93 <micheal.egan93@gmail.com>
2026-07-25 21:16:38 -0700

    made changes to index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,22 @@
+import { env } from "./config/env";
 import express from "express";
+import cookieParser from "cookie-parser";
+import { prisma } from "./lib/prisma";
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cookieParser());
+
+//const PORT = process.env.PORT || 4000;
 
 app.get("/health", async (_req, res) => {
     res.json({ status: "ok", });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.use("/api/auth", authRoutes);
+
+app.listen(env.PORT, () => {
+    console.log(`Server is running on http://localhost:${env.PORT}`);
 });
