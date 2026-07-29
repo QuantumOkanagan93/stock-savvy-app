67b101aef5c727de1aeabbf5c8dae151c32b0528
QuantumOkanagan93 <micheal.egan93@gmail.com>
2026-07-28 23:21:47 -0700

    moved to types directory

---
+++ client/src/types/stocks.ts
@@ -0,0 +1,23 @@
+export type Exchange = "NYSE" | "TSX";
+
+export interface SearchResult {
+    ticker: string;
+    companyName: string;
+    exchange: Exchange;
+}
+
+export interface Quote {
+    symbol: { ticker: string; exchange: Exchange};
+    currentPrice: number;
+    openPrice: number;
+    previousClose: number;
+    percentChangeToday: number;
+    timestamp: number;
+}
+
+export interface SearchHistoryEntry {
+    id: string;
+    ticker: string;
+    exchange: Exchange;
+    createdAt: string;
+}
