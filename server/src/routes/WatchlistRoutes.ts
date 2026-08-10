84e922254a5b0f9b203d149d1a395c93a28a1e8b
QuantumOkanagan93 <micheal.egan93@gmail.com>
2026-08-09 20:17:58 -0700

    Added Getting Watchlist With Data to Routes

--- server/src/routes/WatchlistRoutes.ts
+++ server/src/routes/WatchlistRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { requireAuth } from "../middleware/authMiddleware";
-import { getWatchlist, addToWatchlist, removeFromWatchlist } from "../controllers/WatchlistController";
+import { getWatchlist, getWatchlistWithData, addToWatchlist, removeFromWatchlist } from "../controllers/WatchlistController";
 
 const router = Router();
 
 router.use(requireAuth);
 
 router.get("/", getWatchlist);
+router.get("/with-data", getWatchlistWithData);
 router.post("/", addToWatchlist);
 router.delete("/:itemId", removeFromWatchlist);
 
